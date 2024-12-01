5bd547d603e38c18550b54784672524dd6019f4d
tunki1201 <tunki1201@gmail.com>
2024-12-01 22:54:02 +0800

    FEAT/update-report-api

--- app/utils/scrapping_engine.py
+++ app/utils/scrapping_engine.py
@@ -27,7 +27,7 @@ def fetch_company_info(url: str, company_name: str, company_background: str):
     # Prepare the headers
     headers = {
         "Content-Type": "application/json",
-        "Authorization": f"Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjM2MjgyNTg2MDExMTNlNjU3NmE0NTMzNzM2NWZlOGI4OTczZDE2NzEiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiIzMjU1NTk0MDU1OS5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsImF1ZCI6IjMyNTU1OTQwNTU5LmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTEzMzU2NjA5MzQ3NDA1Mjg4NDM2IiwiaGQiOiJ1c2VrYXNlLmFpIiwiZW1haWwiOiJ2aW5jZW50QHVzZWthc2UuYWkiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXRfaGFzaCI6IlhaaDFMVjRIWG1HQm9rMEd1V0xnOEEiLCJpYXQiOjE3MzI4ODI5NDQsImV4cCI6MTczMjg4NjU0NH0.l_Tl_6nYQhDCQsRld63rQo88AL9MwqyXrjHy48j4-PeiaVFYF4pvKAlLx_qXq0oL9Xxn4TwbElQ6zLDrF6XDQ8w0JSbUDWfOl2zD9wb1xSWDzmNtETWCfx4PH7a0JStwqx0SWt9K2UdxmThPW8AO9vdLz2gQRX82hZ7jzsSisyWOvDKczIP0ofMta-SWdAUi4ISqD_L9QZghgntZjvwQiUJD3KA19BH8yE9YFN6OJfJQ696MAS97nwfPQOV7LYsQWHYFZ6hnWd_POgihDN-fcpyPmQ4THEA_2oViLmCmrYQcCP7OhWeGwWXUEaHf91CZcfum6Ja0sEfhR1OQPKhn8Q",  # Use the loaded API token
+        "Authorization": f"Bearer {API_TOKEN}",  # Use the loaded API token
     }
 
     try:
